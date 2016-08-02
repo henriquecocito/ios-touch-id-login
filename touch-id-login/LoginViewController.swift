51ed755d2ab035229aae731348b17e310a37cb58
Henrique Cocito <henriquecocito@gmail.com>
2016-08-01 23:27:45 -0300

    Initial commit

---
+++ touch-id-login/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginViewController.swift
+//  touch-id-login
+//
+//  Created by Henrique Cocito on 8/1/16.
+//  Copyright (c) 2016 Henrique Cocito. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func loginAction(sender: UIButton) {
+    }
+}
+
